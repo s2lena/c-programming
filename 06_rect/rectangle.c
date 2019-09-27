b26c361c7328ed2ed7201c368ee268f41093ca43
Coursera Learner <nobody@nowhere.nul>
2019-09-27 08:43:44 +0000

    Did assignment 0

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -16,17 +16,71 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
-
-
+struct rect_t {
+int x;
+int y;
+int width;
+int height;
+};
+typedef struct rect_t rectangle;
 rectangle canonicalize(rectangle r) {
   //WRITE THIS FUNCTION
+  if (r.width > 0 && r.height > 0) {
+    r.x = r.x; }
+  if (r.width < 0 && r.height > 0) {
+    r.width = r.width * (-1);
+    r.x = r.x - r.width; }
+  if (r.width > 0 && r.height < 0) {
+    r.height = r.height * (-1);
+    r.y = r.y - r.height; }
+  if (r.width < 0 && r.height < 0) {
+    r.width = r.width * (-1);
+    r.height = r.height * (-1);
+    r.x = r.x - r.width;
+    r.y = r.y - r.height; }
   return r;
 }
 rectangle intersection(rectangle r1, rectangle r2) {
-  //WRITE THIS FUNCTION
-  return r1;
+  //WRITE THIS FUNCtion
+  rectangle r;
+  r1 = canonicalize(r1);
+  r2 = canonicalize(r2);
+  if (r1.x - r2.x >= r2.width || r2.x - r1.x >= r1.width || r1.y - r2.y > r1.height || r2.y - r1.y > r2.height) {
+    if ((r1.y + r1.height) == r2.y || (r2.y + r2.height) == r1.y) {
+      r.x = max(r1.x, r2.x);
+      r.y = max(r1.y, r2.y);
+      if ( r1.x > r2.x ) {
+	r.width = (r2.x + r2.width) - r1.x; }
+      else { r.width = (r1.x + r1.width) - r2.x; }
+      if ( r1.y > r2.y ) {
+	r.height = (r2.y + r2.height) - r1.y; }
+      else {
+	r.height = (r1.y + r1.height) - r2.y;}
+    }
+    else {
+    r.x = 0;
+    r.y = 0;
+    r.width = 0;
+    r.height = 0;
+    }
+  }
+  else {
+	r.x = max(r1.x, r2.x);
+	r.y = max(r1.y, r2.y);
+	if ( r1.x > r2.x ) {
+	  r.width = (r2.x + r2.width) - r1.x; }
+        else { r.width = (r1.x + r1.width) - r2.x; }
+        if ( r1.y > r2.y ) {
+          r.height = (r2.y + r2.height) - r1.y; }
+        else {
+          r.height = (r1.y + r1.height) - r2.y;}
+    }
+  if (r1.x < 0 && r1.x > r2.x && r1.width < r2.width && r1.height < r2.height) {
+    r = r1; }
+  if (r2.x < 0 && r2.x > r1.x && r2.width < r1.width && r2.height < r1.height) {
+    r = r2; }
+  return r;
 }
-
 //You should not need to modify any code below this line
 void printRectangle(rectangle r) {
   r = canonicalize(r);

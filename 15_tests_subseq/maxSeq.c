42d0e06b46fc3836bfc79fbd008a5e50f2515612
Coursera Learner <nobody@nowhere.nul>
2019-10-21 16:17:57 +0000

    Did assignment 0

--- 15_tests_subseq/maxSeq.c
+++ 15_tests_subseq/maxSeq.c
@@ -11,12 +11,17 @@ size_t maxSeq(int* array, size_t n) {
   int length = 0;
   int max_length = 0;
 
-  for (int i = 0; i < n; i++) {
+  if (n == 0) {
+    return 0;
+  }
+
+  for (int i = 0; i < n - 1; i++) {
     length++;
     if (array[i] >= array[i + 1]) {
       max_length = max(max_length, length);
       length = 0;
-    }
+      }
   }
+  max_length = max(max_length, length + 1);
   return max_length;
 }
--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -1,42 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-size_t maxSeq (int * array, size_t n);
 
-int main() {
-  int test1[] = {4,5,-4,-3,-2,-1,1};
-  if(maxSeq(test1, 6) != 4) {
-    printf("Failed on 1");
+size_t maxSeq(int* array, size_t n);
+
+void testMaxSeq(int i, int* array, size_t n, size_t expect) {
+  if (maxSeq(array, n) != expect) {
+    printf("Failed on %d\n", i);
     exit(EXIT_FAILURE);
   }
+}
+  
+int main() {  
+  int test1[] = {};
+  testMaxSeq(1, test1, 0, 0);
+
   int test2[] = {1};
-  if(maxSeq(test2, 1) != 1) {
-    printf("Failed on 2");
-    exit(EXIT_FAILURE);
-  }
-  int test3[] = {};
-  if(maxSeq(test3, 0) != 0) {
-    printf("Failed on 3");
-    exit(EXIT_FAILURE);
-  }
-  int test4[] = {4,3,2,1,0};
-  if(maxSeq(test1, 5) != 1) {
-    printf("Failed on 4");
-    exit(EXIT_FAILURE);
-  }
-  int test5[] = {4,5,-4,-3,-2,-1,1};
-  if(maxSeq(test1, 6) != 4) {
-    printf("Failed on 5");
-    exit(EXIT_FAILURE);
-  }
-  int test6[] = {1,2,3,4,5,6};
-  if(maxSeq(test1, 6) != 3) {
-    printf("Failed on 6");
-    exit(EXIT_FAILURE);
-  }
-  int test7[] = {1,2,3,6,6,7};
-  if(maxSeq(test1, 6) != 4) {
-    printf("Failed on 7");
-    exit(EXIT_FAILURE);
-  }
-  return 0;
+  testMaxSeq(2, test2, 1, 1);
+
+  int test3[] = {1, 2};
+  testMaxSeq(3, test3, 2, 2);
+
+  int test4[] = {2, 1};
+  testMaxSeq(4, test4, 2, 1);
+
+  int test5[] = {1, 1};
+  testMaxSeq(5, test5, 2, 1);
+
+  int test6[] = {1, 2, 4};
+  testMaxSeq(6, test6, 3, 3);
+
+  int test7[] = {4, 2, 1};
+  testMaxSeq(7, test7, 3, 1);
+
+  int test8[] = {1, 2, 1};
+  testMaxSeq(8, test8, 3, 2);
+
+  int test9[] = {-4, -2, -1};
+  testMaxSeq(9, test9, 3, 3);
+  
+  int test10[] = {1, 2, 4, 1, 2};
+  testMaxSeq(10, test10, 5, 3);
+
+  int test11[] = {1, 2, 1, 2, 4};
+  testMaxSeq(11, test11, 5, 3);
+
+  int test12[] = {1, 2, -1, -2, -4};
+  testMaxSeq(12, test12, 5, 2);
+  
+  return EXIT_SUCCESS;
 }

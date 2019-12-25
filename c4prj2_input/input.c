d3f3989b2b3b270dcd5fd42032c270ad9746bdb9
Coursera Learner <nobody@nowhere.nul>
2019-12-25 04:34:15 +0000

    Did assignment c4prj2

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -5,23 +5,28 @@
 #include <string.h>
 
 deck_t* hand_from_string(const char* str, future_cards_t* fc) {
-  deck_t * d = malloc(sizeof(deck_t));
-  d->cards = NULL; card_t c = {.value = 0, .suit = 0};
+  deck_t* d = malloc(sizeof(deck_t));
+  d->cards = NULL;
+  card_t c = {.value = 0, .suit = 0};
   int numcards = 0;
   int j = 0;
-  char * card = malloc(2 * sizeof(char));
+  char* card = malloc(2 * sizeof(char));
   for (int i = 0; i < strlen(str); i++) {
     if (str[i] != ' ') {
       card[j] = str[i];
       j++;
-    }
-    else {
+    } else {
       j = 0;
-      numcards++;
-      d->cards = realloc(d->cards, numcards * sizeof(card_t));
-      c = card_from_letters(card[0], card[1]);
-      d->cards[numcards - 1]->suit = c.suit;
-      d->cards[numcards - 1]->value = c.value;
+      if (c.suit != NUM_SUITS) {
+	numcards++;
+	d->cards = realloc(d->cards, numcards * sizeof(card_t));
+	c = card_from_letters(card[0], card[1]);
+	d->cards[numcards - 1]->suit = c.suit;
+	d->cards[numcards - 1]->value = c.value;
+      } else {
+	add_empty_card(d);
+	add_future_card(fc, (size_t)card[0] , d->cards[d->n_cards - 1]);
+      }
     }
   }
   return d;
@@ -34,7 +39,7 @@ deck_t** read_input(FILE* f, size_t* n_hands, future_cards_t* fc) {
   size_t sz = 0;
   while (getline(&curr, &sz, f) >= 0) {
     lines++;
-    arr_hand  = realloc(arr_hand, lines * sizeof(deck_t *));
+    arr_hand = realloc(arr_hand, lines * sizeof(deck_t*));
     char* p = strchr(curr, '\n');
     if (p != NULL) {
       *p = '\0';
@@ -45,6 +50,6 @@ deck_t** read_input(FILE* f, size_t* n_hands, future_cards_t* fc) {
     curr = NULL;
   }
   free(curr);
-  * n_hands = lines;
+  *n_hands = lines;
   return arr_hand;
 }

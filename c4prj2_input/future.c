0cba140ec2dbefd8bfe1d3cc231a7a276d3ad443
Coursera Learner <nobody@nowhere.nul>
2019-12-25 10:23:30 +0000

    Did assignment c4praj2

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -28,7 +28,7 @@ void future_cards_from_deck(deck_t* deck, future_cards_t* fc) {
   }
   for (int i = 0; i < deck->n_cards; i++) {
     for (int j = 0; j < fc->decks[i].n_cards; i++) {
-      fc->decks[i].cards[j] = &deck->cards[i];
+      fc->decks[i].cards[j] = deck->cards[i];
     }
   }
 }
